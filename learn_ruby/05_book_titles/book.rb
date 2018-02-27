818e4e5b95461b1314d4748e134cdaeed50d1855
Khoi Nguyen <khnaero@gmail.com>
2018-02-26 23:53:05 -0600

    Add solution to book.rb

--- learn_ruby/05_book_titles/book.rb
+++ learn_ruby/05_book_titles/book.rb
@@ -1,3 +1,24 @@
 class Book
-# write your code here
+  attr_reader :title
+  # def title
+  #   @title
+  # end
+
+  def title=(str)
+    phrase = str.split
+    phrase = [phrase[0].capitalize] + 
+    phrase[1..-1].map do |word|
+      little = ['the', 'and', 'or', 'in', 'of', 'a', 'an']
+      if little.include? word
+        word
+      else
+        word.capitalize
+      end
+    end
+    @title = phrase.join(' ')
+  end
 end
+
+# @book = Book.new
+# @book.title = "war and peace"
+# p @book.title
